aaa155a0f8fd952d317e542de629f9480240da3a
Daniel Ribeiro <42726779+tsunas4@users.noreply.github.com>
2020-03-10 14:34:58 -0300

    Iniciando um projeto no git

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        //
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["mensagem"] = "Hello " + name;
+            ViewData["numTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
